9c40be30247f22ae59ef684efaf5ec3f312e676e
xykro <nico.schwab@bluewin.ch>
2023-05-12 17:45:36 +0200

    V2.2.2, trying some bug fixing

--- save_scroll_position_moodle_tampermonkey.js
+++ save_scroll_position_moodle_tampermonkey.js
@@ -2,7 +2,7 @@
 // @name            Save Scroll Position in Moodle Boost Design
 // @namespace       Moodle
 // @author          ScN
-// @version         2.2.1
+// @version         2.2.2
 // @description     Saves the scroll position in Moodle courses with Moodle using the new Boost design. This simplifies navigating into Moodle items and returning to the same scroll location upon back navigation.
 // @match           https://moodle.bbbaden.ch/course/view.php*
 // @downloadURL     https://raw.githubusercontent.com/BBBelektronik/moodle-scrollpos/main/save_scroll_position_moodle_tampermonkey.js
@@ -47,7 +47,7 @@ function restoreSessionScrollPosition(courseId) {
     var storedPosition = getSessionScrollPosition(courseId);
     if (storedPosition !== null) {
         page.scrollTop = storedPosition;
-        // console.log("Scrollposition für Kurs", courseId, "wiederhergestellt:", storedPosition);
+        console.log("Scrollposition für Kurs", courseId, "wiederhergestellt:", storedPosition);
     }
 }
 
